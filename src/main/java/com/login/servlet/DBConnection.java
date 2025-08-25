2e945e824beffaa1b7ff8a6383cf2c2e7b4c2e7f
Er Rehan Ansari <rehanansaridard@gmail.com>
2025-08-25 16:41:20 +0530

    Database Connection utility class

--- src/main/java/com/login/servlet/DBConnection.java
+++ src/main/java/com/login/servlet/DBConnection.java
@@ -4,21 +4,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 
 public class DBConnection {
-	
-	private static final String url="jdbc:mysql://localhost:3306/logininfo";
-	private static final String uid="root";
-	private static final String password="Rehan@123";
-	
-	public static Connection getConnection() {
-		Connection con=null;
-		
-		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-			con=DriverManager.getConnection(url,uid,password);
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-		return con;
-	}
 
+    private static final String url = "jdbc:mysql://localhost:3306/logininfo";
+    private static final String uid = "root";
+    private static final String password = "Rehan@123";
+
+    public static Connection getConnection() {
+        Connection con = null;
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            con = DriverManager.getConnection(url, uid, password);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return con;
+    }
 }
